904ecad53d695b01bf6d06cddcd68780c9a2a38e
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-07-06 20:16:30 +0000

    Added Home app to Installed apps on settings project level as well as url in urls project level

--- Coffee_Drop_Ecommerce/settings.py
+++ Coffee_Drop_Ecommerce/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +60,10 @@ ROOT_URLCONF = 'Coffee_Drop_Ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allatuh'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
