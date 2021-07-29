80f5bb20170b279887caecb248b0d7be4b41c0b4
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-07-29 14:40:53 +0000

    Added newsletters templates and urls. Updated base.css as well as comented base.html adn changed footer.

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
 
+from .models import NewsletterUser
+from .forms import NewsletterUserSignUpForm
 # Create your views here.
+
+# Sign Up
+def newsletter_signup(request):
+    form = NewsletterUserSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewsletterUser.objects.filter(email=instance.email).exists():
+            messages.warning(request, 
+            'Your email already exists in our database', 
+            'alert alert-warning alert-dismissible')
+        else:
+            instance.save()
+            messages.success(request,
+                            'Your email has been submited to the database',
+                            'alert alert-success alert-dismissible')
+            return redirect(reverse('home_blog'))
+
+    context = {
+        'form': form,
+    }
+    template = 'newsletters/sign_up.html'
+    return render(request, template, context)
+
+
+# Unsubscribe
+def newsletter_unsubscribe(request):
+    form = NewsletterUserSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewsletterUser.objects.filter(email=instance.email).exists():
+            NewsletterUser.objects.filter(email=instance.email).delete()
+            messages.success(request,
+                            'Your email has been removed!',
+                            'alert alert-success alert-dismissible')
+        else:
+            messages.warning(request,
+                            'Your email is not in the database!',
+                            'alert alert-warning alert-dismissible')
+
+    context = {
+        'form': form,
+    }
+    template = 'newsletters/unsubscribe.html'
+    return render(request, template, context)
