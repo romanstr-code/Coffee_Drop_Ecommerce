6f49cf2e2a789e4feba9f15659569c9869df60a5
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-07-30 13:11:58 +0000

    Set up settings in project level settings for sending real emails

--- Coffee_Drop_Ecommerce/settings.py
+++ Coffee_Drop_Ecommerce/settings.py
@@ -110,8 +110,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email' # Authentication with username or emails
 ACCOUNT_EMAIL_REQUIRED = True # Email required for registration
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory' # Email verification is mandatory
@@ -216,7 +214,21 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = '	Coffee_Drop_Ecommerce@example.com'
+
+
+if 'DEVELOPEMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = '	Coffee_Drop_Ecommerce@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
