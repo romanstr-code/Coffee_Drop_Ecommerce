e608b4ae185d78f1e2facdf888f783a447e3e11a
romanstr-code <romanstrogoteanu1990@gmail.com>
2021-07-30 10:56:35 +0000

    Added Cache control

--- Coffee_Drop_Ecommerce/settings.py
+++ Coffee_Drop_Ecommerce/settings.py
@@ -185,6 +185,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'romanstr-coffee-drop-ecommerce'
     AWS_S3_REGION_NAME = 'eu-west-1'
@@ -201,7 +207,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
+
 
 # Calculates Delivery coasts # Stripe # Webhooks
 FREE_DELIVERY_THRESHOLD = 50
